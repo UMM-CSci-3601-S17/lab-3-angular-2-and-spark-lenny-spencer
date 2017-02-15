02065c1073852b08b5c9848855454a406763227d
Dogxx000 <scot0530@morris.umn.edu>
2017-02-15 16:37:54 -0600

    Mention todo in higher level files Issue #4

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { KittensComponent } from './kittens/kittens.component';
 import {UserListComponent} from "./users/user-list.component";
+import {TodoListComponent} from "./todos/todo-list.component";
 
 // Route Configuration
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'kittens', component: KittensComponent },
-    { path: 'users', component: UserListComponent }
+    { path: 'users', component: UserListComponent },
+    { path: 'todos', component: TodoListComponent}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
